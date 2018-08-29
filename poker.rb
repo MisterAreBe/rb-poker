b06f01e66e5c28cd7f5c15c6680e2aea2174aaf4
Ricky Collins <richardcollins52317@gmail.com>
2018-08-29 15:47:47 -0400

    test pass

--- poker.rb
+++ poker.rb
@@ -1,6 +1,12 @@
 def deck(cards)
-    cards.each do |v|
-        v.upcase!
+    if cards.is_a?(String)
+        return "random"
+    elsif cards.is_a?(Array)
+        cards.each do |v|
+            v.upcase!
+        end
+        return cards
+    else
+        return false
     end
-    cards
 end
