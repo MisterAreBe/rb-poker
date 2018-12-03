4cbac5ac9fb760f81d0ef7bca417121bb50021f1
Ricky Collins <richardcollins52317@gmail.com>
2018-12-03 11:36:13 -0500

    test passed

--- card.rb
+++ card.rb
@@ -2,5 +2,12 @@ class Cards
     def initialize(card)
         face = {"T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14}
         suit = {"S" => "Spades", "C" => "Clubs", "D" => "Diamonds", "H" => "Hearts"}
+        face.each_pair do |key, value|
+            unless card[0].is_a?(Integer) && card[0] == key
+                @card_value = value
+            end
+            @card_value = card[0].to_i
+        end
     end
+    attr_reader :card_value
 end
