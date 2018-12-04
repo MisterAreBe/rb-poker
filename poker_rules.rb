aa14b7bbc5abb1663251a71bf1360f73d6901a63
Ricky Collins <richardcollins52317@gmail.com>
2018-12-04 10:40:24 -0500

    test passed, shortend that call of hand from game.deck.hands.playrs to game.players

--- poker_rules.rb
+++ poker_rules.rb
@@ -1,8 +1,11 @@
 require_relative "poker.rb"
 class Game
-    def initialize()
+    def initialize(num_of_players=2)
         @deck = Poker_deck.new()
+        @deck.deal_out_cards(num_of_players)
+        @players = @deck.hands.players #to easly call on the hands
     end
     
+    attr_reader :players
     attr_reader :deck
 end
